b52a86802bfddd5d0d04a82fd0c683d466bda47f
FedericoComoglio <federico.comoglio@gmail.com>
2021-04-21 08:35:02 +0200

    Added roxygen documentation for package, related to #1

---
+++ R/dupiR-package.R
@@ -0,0 +1,25 @@
+#' Bayesian inference from count data using discrete uniform priors
+#' 
+#' @name dupiR-package
+#' 
+#' @description This package allows to infer population sizes using a binomial likelihood 
+#' and least informative discrete uniform priors.
+#' 
+#' @author Federico Comoglio \email{federico.comoglio@@gmail.com}
+#' @author Maurizio Rinaldi
+#' 
+#' @aliases dupiR-package dupiR
+#' 
+#' @references 
+#' 
+#' Comoglio F, Fracchia L, Rinaldi M (2013) Bayesian Inference from Count Data 
+#' Using Discrete Uniform Priors. PLoS ONE 8(10): e74388
+#'
+#' @docType package
+#'
+#' @keywords package
+#' 
+NULL
+
+
+
