c0296f4e80d208ebb83998a4631afe54c2b72531
FedericoComoglio <federico.comoglio@gmail.com>
2021-04-23 10:39:49 +0200

    Improved slot description (code tag); prepared for documenting methods in class doc

--- R/class.R
+++ R/class.R
@@ -3,28 +3,28 @@
 #'
 #' @slot counts integer vector of counts (required)
 #' @slot fractions numeric vector of sampling fractions (required)
-#' @slot n_start start of prior support range. If omitted and total `counts` greater than zero,
-#' computed as 0.5 * `mle`, where `mle` is the maximum likelihood estimate of the population size
-#' @slot n_end end of prior support range. If omitted and total `counts` greater than zero,
-#' computed as 2 * `mle`, where `mle` is the maximum likelihood estimate of the population size
-#' @slot f_product product of (1-`fractions`)
+#' @slot n_start start of prior support range. If omitted and total \code{counts} greater than zero,
+#' computed as 0.5 * \code{mle}, where \code{mle} is the maximum likelihood estimate of the population size
+#' @slot n_end end of prior support range. If omitted and total \code{counts} greater than zero,
+#' computed as 2 * \code{mle}, where \code{mle} is the maximum likelihood estimate of the population size
+#' @slot f_product product of (1-\code{fractions})
 #' @slot mle maximum likelihood estimate of the population size (ratio between total counts and total sampling fraction)
 #' @slot norm_constant normalization constant
 #' @slot posterior numeric vector of posterior probabilities over the prior support
-#' @slot map_p maximum of `posterior` probability
+#' @slot map_p maximum of \code{posterior} probability
 #' @slot map_index index of prior support corresponding to the maximum a posteriori
 #' @slot map maximum a posteriori of population size
 #' @slot q_low lower bound of the credible interval
 #' @slot q_low_p probability of the lower bound of the credible interval
-#' @slot q_low_index index of the prior support corresponding to `q_low`
-#' @slot q_low_cum_p cumulative posterior probability from `n1` to `q_low` (left tail)
+#' @slot q_low_index index of the prior support corresponding to \code{q_low}
+#' @slot q_low_cum_p cumulative posterior probability from \code{n_start} to \code{q_low} (left tail)
 #' @slot q_up upper bound of the credible interval
 #' @slot q_up_p probability of the upper bound of the credible interval
-#' @slot q_up_index index of the prior support corresponding to `q_high`
-#' @slot q_up_cum_p cumulative posterior probability from `q_high` to `n2` (right tail)
+#' @slot q_up_index index of the prior support corresponding to \code{q_high}
+#' @slot q_up_cum_p cumulative posterior probability from \code{q_high} to \code{n_end} (right tail)
 #' @slot gamma logical, TRUE if posterior computed using a Gamma approximation
 #'
-#' @note The `posterior` slot contains either the PMF or a logical value used to 
+#' @note The \code{posterior} slot contains either the PMF or a logical value used to 
 #' compute posterior parameters with a Gamma approximation (see reference for details)
 #' @note Lower and upper bounds of the credibile interval are computed at a default
 #' confidence level of 95%
@@ -32,14 +32,12 @@
 #' 
 #' @references Comoglio F, Fracchia L and Rinaldi M (2013) 
 #' Bayesian inference from count data using discrete uniform priors. 
-#' [PLoS ONE 8(10): e74388](http://www.plosone.org/article/info%3Adoi%2F10.1371%2Fjournal.pone.0074388)
+#' \href{https://journals.plos.org/plosone/article?id=10.1371/journal.pone.0074388}{PLoS ONE 8(10): e74388}
 #'
 #' @author Federico Comoglio
 #'
-#' @seealso [dupiR::new_counts()]
-#'
 #' @examples 
-#' constructor:
+#' # constructor:
 #' # create an object of class 'Counts'
 #' new_counts(counts = c(30, 35), fractions = c(0.075, 0.1))
 #'
